b60c57176cc7a95efad180751a7d30194ffc563d
linfengwei1 <1520415662@qq.com>
2020-04-06 17:24:41 +0800

    只传src

--- src/main/java/com/great/DemoApplication.java
+++ src/main/java/com/great/DemoApplication.java
@@ -1,5 +1,6 @@
 package com.great;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
