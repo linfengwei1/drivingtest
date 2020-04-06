fe212ef257c7d63b0e637703b002005a6e9a0231
linfengwei1 <1520415662@qq.com>
2020-04-06 17:48:40 +0800

    注意不要穿.idea文件夹和target文件夹

--- src/main/java/com/great/DemoApplication.java
+++ src/main/java/com/great/DemoApplication.java
@@ -15,6 +15,7 @@ public class DemoApplication extends SpringBootServletInitializer
 	}
 	public static void main(String[] args) {
 		System.out.println("我来打个酱油");
+		System.out.println("注意不要提交.idea文件夹和target文件夹");
 		SpringApplication.run(DemoApplication.class, args);
 	}
 }
