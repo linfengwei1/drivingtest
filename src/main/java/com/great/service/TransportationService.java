49ca6b28026dbb4ef5892a09a44140ced68cb678
CBin286 <ye13286@163.com>
2020-04-08 10:45:35 +0800

    ycb 2020/04/08 11:00 增加运管所需的类 增加objectResult类，用于封装layui表格所需要的数据，适用于所有实体对象。 只需要传入code,count,list<目标对象>

---
+++ src/main/java/com/great/service/TransportationService.java
@@ -0,0 +1,15 @@
+package com.great.service;
+
+/**
+ * ClassName: TransportationService <br/>
+ * Description: <br/>
+ * date: 2020/4/8 10:35<br/>
+ *
+ * @author lenovo<br />
+ * @since JDK 1.8
+ */
+public interface TransportationService {
+
+
+
+}
---
+++ src/main/java/com/great/service/serviceimpl/TransportationServiceImp.java
@@ -0,0 +1,16 @@
+package com.great.service.serviceimpl;
+
+import com.great.service.TransportationService;
+
+/**
+ * ClassName: TransportationServiceImp <br/>
+ * Description: <br/>
+ * date: 2020/4/8 10:35<br/>
+ *
+ * @author lenovo<br />
+ * @since JDK 1.8
+ */
+public class TransportationServiceImp implements TransportationService {
+
+
+}
