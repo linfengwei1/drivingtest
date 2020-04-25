f8b520ba80cd7a67781c270c394318d28af877a6
OsAtNi <498974209@qq.com>
2020-04-25 23:35:37 +0800

    4-25 王良德补全功能2

---
+++ src/main/java/com/great/interceptor/CoachLoginConfig.java
@@ -0,0 +1,20 @@
+package com.great.interceptor;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CoachLoginConfig implements WebMvcConfigurer
+{
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        //注册TestInterceptor拦截器
+        InterceptorRegistration registration = registry.addInterceptor(new CoachLoginInterceptor());
+        registration.addPathPatterns("/coach/**");                      //所有驾校操作路径都被拦截
+        registration.excludePathPatterns(                         //添加不拦截路
+                "/coach/login"//驾校登录
+        );
+    }
+}
